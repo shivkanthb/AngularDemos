08036540c29a11696d3f8cd56c83b45c55acfeaf
shivkanth <shivkanthb@gmail.com>
2015-04-14 03:42:56 -0700

    jsonp fetch and teaser images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 "use strict";
 
-var app = angular.module('dabbble', []);
+var app = angular.module('dabbble', ['dabbble.controllers']);
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function AppCtrl($scope) {
-    $scope.name = "World!!";
-    $scope.updateName = function() {
-        $scope.name = "World Updated";
-    }
-}
+//function AppCtrl($scope) {
+//    $scope.name = "World!!";
+//    $scope.updateName = function() {
+//        $scope.name = "World Updated";
+//    }
+//}
+
+var controllers = angular.module('dabbble.controllers', []);
+
+controllers.controller('AppCtrl', function($scope) {
+    $scope.name="Module";
+});
+
+controllers.controller('ShotsListCtrl', function($scope, $http) {
+    
+    
+    $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+        $scope.list=data.data;
+        console.log(data);
+    })
+    
+    
+});
